ca60e52c3e6591b39a2246b3b8ab185c7edd6ee0
Hunghayom <124016028+Hunghayom@users.noreply.github.com>
2023-04-13 05:16:38 +0700

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-g++ Texture_box.cpp Game.cpp main.cpp -o  game -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
+g++ Player.cpp Tilemap.cpp Texture_box.cpp Game.cpp main.cpp -o  game -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
